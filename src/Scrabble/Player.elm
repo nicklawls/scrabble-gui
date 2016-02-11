ffd069718d8ee8537240bc9700ebe3dee29f66d6
Nick Lawler <lawler145@gmail.com>
2016-02-11 02:03:00 -0800

    add json encoding for Player

--- src/Scrabble/Player.elm
+++ src/Scrabble/Player.elm
@@ -2,9 +2,13 @@ module Scrabble.Player where
 
 import Json.Decode exposing (Decoder, (:=))
 import Scrabble.Board as Board exposing (Rack, Score)
-
+import Json.Encode exposing (Value)
 
 type PlayerType = Human | AI
+
+
+encodePlayerType : PlayerType -> Value
+encodePlayerType = Json.Encode.string << toString
 
 
 playerType : Decoder PlayerType
@@ -28,6 +32,17 @@ type alias Player =
     , playerScore : Score
     , playerId : Int
     }
+
+
+encodePlayer : Player -> Value
+encodePlayer t =
+    Json.Encode.object
+        [ ("playerType", encodePlayerType t.playerType)
+        , ("playerName", Json.Encode.string t.playerName)
+        , ("playerRack", Board.encodeRack t.playerRack)
+        , ("playerScore", Json.Encode.int t.playerScore)
+        , ("playerScore", Json.Encode.int t.playerId)
+        ]
 
 
 player : Decoder Player
