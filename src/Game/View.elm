6faa63809e64b5b0a4f9ab2ea2d607b6b8effa53
Nick Lawler <lawler145@gmail.com>
2016-02-17 21:08:44 -0800

    stub out view components, import Model unqualified, enforce 2 newline rule

--- src/Game/View.elm
+++ src/Game/View.elm
@@ -1,22 +1,39 @@
 module Game.View where
 
 
-import Game.Model as Game
+import Game.Model as Game exposing (Model)
 import Game.Update as Game exposing (Action)
 import Html exposing (Html, div, text)
 import Signal exposing (Address)
+
 
 type alias Context =
     { playerId : Game.PlayerId }
 
 
-view : Context -> Address Action -> Game.Model -> Html
-view context address {gamePlayers, gameBoard, gameBag, gameTurns} =
+-- display the game
+view : Context -> Address Action -> Model -> Html
+view context address model =
     div []
-        [ div [] [ text <| toString gamePlayers ]
-        , div [] [ text <| toString gameBoard ]
-        , div [] [ text <| toString gameBag ]
-        , div [] [ text <| toString gameTurns ]
+        [ viewScoreboard model
+        , viewBoard model
+        , viewRack context model
         ]
 
 
+-- Display the two players and their scores
+viewScoreboard : Model -> Html
+viewScoreboard model =
+    Debug.crash "implement scoreboard"
+
+
+-- Display the board
+viewBoard : Model -> Html
+viewBoard model =
+    Debug.crash "implement board"
+
+
+-- display the local player's personal rack
+viewRack : Context -> Model -> Html
+viewRack context model =
+    Debug.crash "implement rack"
